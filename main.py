7a1df0cd6f674843e9e7b8c2b6ce71b315b4a6fb
yaroslav <redyar55@gmail.com>
2023-11-15 16:57:26 +0300

    Upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, render_template, redirect
+from flask import send_from_directory
 import os
 import PyPDF2
 import re
@@ -18,20 +19,24 @@ def extract_links_from_pdf(file_path):
             links.extend(found_urls)
     return links
 
+@app.route('/view_pdf/<filename>')
+def view_pdf(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=False)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Получаем список уже загруженных файлов
+    uploaded_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', uploaded_files=uploaded_files)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_pdf():
     if 'file' not in request.files:
         return redirect(request.url)
-
     file = request.files['file']
-
     if file.filename == '':
         return redirect(request.url)
-
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
@@ -40,7 +45,12 @@ def upload_pdf():
 
 @app.route('/download/<filename>')
 def download(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    # Ищем ссылки в загруженном PDF-файле
+    links = extract_links_from_pdf(file_path)
+
+    return render_template('links.html', filename=filename, links=links)
 
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
